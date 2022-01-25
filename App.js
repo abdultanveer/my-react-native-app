f95c38301ba4479b8012b66000ab2f3fcce85425
abdul <abdul.tanveer@gmail.com>
2022-01-25 12:34:31 +0530

    scroll view

--- App.js
+++ App.js
@@ -1,38 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
 
-  const [name,setName] = useState('abdul')
-  const [person, setPerson] = useState({name: 'tanveer', age: 123})
+  const [people, setPeople] = useState(
+    [
+      { name: 'abdul', key: '1' },
+      { name: 'ansari', key: '2' },
+      { name: 'abhinav', key: '3' },
+      { name: 'niket', key: '4' },
+      { name: 'harshit', key: '5' },
+      { name: 'abdul', key: '6' },
+      { name: 'ansari', key: '7' },
+      { name: 'abhinav', key: '8' },
+      { name: 'niket', key: '9' },
+      { name: 'harshit', key: '10' },
+      { name: 'abdul', key: '11' },
+      { name: 'ansari', key: '12' },
+      { name: 'abhinav', key: '13' },
+      { name: 'niket', key: '14' },
+      { name: 'harshit', key: '15' },
 
-  const [age,setAge] = useState('30')
-
-
-  const clickHandler = () => {
-    setName('ansari')
-  }
+    ]
+  )
 
   return (
     <View style={styles.container}>
-      <Text>My name is {name}</Text>
-      <Text>his name is {person.name} and his age is  {age}</Text>
-      <TextInput 
-      style={styles.input}
-      placeholder = 'enter name'
-      onChangeText = {(val) => setName(val)}
-      />
+    <ScrollView>
+      {
+        people.map((item) => {
+          return (
+            <View key={item.key}>
+              <Text style = {styles.item}>{item.name}</Text>
+            </View>
+          )
+        }
+        )}
+        </ScrollView>
 
-      <TextInput 
-      keyboardType = 'numeric'
-      style={styles.input}
-      placeholder = 'enter age eg 99'
-      onChangeText = {(val) => setAge(val)}
-      />
-      <Button title = 'update state'  onPress = {clickHandler}/>
     </View>
-    
+
   );
 }
 
@@ -44,12 +52,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  boxStyle : {
+  boxStyle: {
     width: 50, height: 50, backgroundColor: 'powderblue'
   },
   input: {
     border: '1px solid',
     borderColor: 'blue',
     padding: 8,
+  },
+  item: {
+    marginTop: 24, 
+    padding: 30, 
+    height: 30 ,
+    backgroundColor: 'pink',
+    fontSize: 24,
   }
 });
